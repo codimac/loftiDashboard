ce8918be996f43dfb4ddd7d26f0953443499d91e
florian <zobeleflorian@gmail.com>
2018-03-17 15:32:33 +0100

    test axios

--- src/containers/FetchData.jsx
+++ src/containers/FetchData.jsx
@@ -10,18 +10,21 @@ export default class FetchData extends React.Component {
     };
   }
 
-  componentDidMount() {
+  handle = () => {
     Http.get('/posts')
-      .then(res => this.setState({posts: res.data}))
+      .then(res => this.setState({ posts: res.data }))
       .catch(err => console.error(err));
   }
 
   render() {
     return (
       <div>
-        {this.state.posts.slice(0, 20).map(p =>
-          <div key={p.id}>{p.id} - {p.title}</div>
-        )}
+        <button onClick={this.handle}>GET</button>
+        <div>
+          {this.state.posts.slice(0, 20).map(p =>
+            <div key={p.id}>{p.id} - {p.title}</div>
+          )}
+        </div>
       </div>
     );
   }
