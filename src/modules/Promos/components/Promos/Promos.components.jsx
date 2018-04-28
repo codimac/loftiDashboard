cda2bf40c1659481d09d4601a622eb7a07ad5fea
Lila <laladu13400@hotmail.fr>
2018-04-28 02:32:20 +0200

    started render one promo

--- src/modules/Promos/components/Promos/Promos.components.jsx
+++ src/modules/Promos/components/Promos/Promos.components.jsx
@@ -1,1 +1,28 @@
 import React from 'react';
+
+class Promos extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      id: 1,
+      label: '2018',
+      students: [
+        {id: 1, name: 'Mary', surname: 'Sue', uersname: 'marysue'}
+      ]
+
+    };
+  }
+
+  render() {
+    const {students} = this.state;
+    const promos = this.state;
+    return (
+      <React.Fragment>
+        {students.id}
+        {promos.label}
+      </React.Fragment>
+    );
+  }
+}
+
+export default Promos;
