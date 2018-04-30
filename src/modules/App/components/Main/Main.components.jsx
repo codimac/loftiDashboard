a33b0218964b13e5d12728131f97e0dc269c53ef
Lila <laladu13400@hotmail.fr>
2018-04-30 10:15:20 +0200

    routing components

--- src/modules/App/components/Main/Main.components.jsx
+++ src/modules/App/components/Main/Main.components.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import Promotion from '@Promos/containers/Details.containers';
+import { Switch, Route } from 'react-router-dom'
+
+import { PrivateRoute } from '@Shared/components/PrivateRoute.components';
+
+import ListUe from '@Courses/components/Ues/ListUe.components';
 
 class Main extends React.Component {
 
   render() {
     return (
-      <Promotion />
+      <React.Fragment>
+        <Switch>
+          <Route exact path='/courses' component={ListUe} />
+          <Route exact path='/promotions' component={Promotion} />
+        </Switch>
+      </React.Fragment>
     );
   }
 
--- src/modules/App/components/Sidebar/Sidebar.components.jsx
+++ src/modules/App/components/Sidebar/Sidebar.components.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ListPromos from '@Promos/containers/List.containers';
 
 import { permissionsSvc } from '@services/permissions.services';
+import { Link } from 'react-router-dom';
+
 import './Sidebar.styles';
 
 class Sidebar extends React.Component {
@@ -10,6 +12,7 @@ class Sidebar extends React.Component {
     return (
       <React.Fragment>
         <ListPromos />
+        <li><Link to='/courses'> Enseignements </Link> </li>
       </React.Fragment>
     );
   }
