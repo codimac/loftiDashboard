bf78d35237273b1265f14c6495fff0a393a7bb36
Lila <laladu13400@hotmail.fr>
2018-04-30 09:40:38 +0200

    function generating any amount of students

--- src/modules/Promos/mocks/details.mocks.jsx
+++ src/modules/Promos/mocks/details.mocks.jsx
@@ -1,4 +1,14 @@
-export default [
-  { id: 1, firstname: 'Florian', lastname: 'Zobèle' },
-  { id: 2, firstname: 'Lila', lastname: 'Gazeau'}
-];
+const createFakeStudents = (number = 25) => {
+  const students = Array(number);
+  for (let i = 0; i < students.length; i++) {
+    students[i] = { id: i, firstname: `name${i}`, lastname: `surname${i}`, uersname: `username${i}`}
+  }
+  return students;
+};
+
+export default createFakeStudents();
+
+// export default [
+//   { id: 1, firstname: 'Florian', lastname: 'Zobèle' },
+//   { id: 2, firstname: 'Lila', lastname: 'Gazeau'}
+// ];
