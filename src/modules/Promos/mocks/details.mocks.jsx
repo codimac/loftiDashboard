0d2a0c3024fcf866915a03cd0d323072b2a7cbea
Lila <laladu13400@hotmail.fr>
2018-04-30 10:17:29 +0200

    missing semicolon - sorry

--- src/modules/Promos/mocks/details.mocks.jsx
+++ src/modules/Promos/mocks/details.mocks.jsx
@@ -1,7 +1,7 @@
 const createFakeStudents = (number = 25) => {
   const students = Array(number);
   for (let i = 0; i < students.length; i++) {
-    students[i] = { id: i, firstname: `name${i}`, lastname: `surname${i}`, uersname: `username${i}`}
+    students[i] = { id: i, firstname: `name${i}`, lastname: `surname${i}`, uersname: `username${i}`};
   }
   return students;
 };
