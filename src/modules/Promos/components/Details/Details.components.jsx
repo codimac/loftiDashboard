089bda6fdf54c2d86a79e0f73d20999e1098f6f4
Lila <laladu13400@hotmail.fr>
2018-05-07 17:03:48 +0200

    improved list

--- src/modules/Promos/components/Details/Details.components.jsx
+++ src/modules/Promos/components/Details/Details.components.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Proptypes from 'prop-types';
 import plus from '@images/icon-plus.png';
+import { Link } from 'react-router-dom';
 import './styles.scss';
 
 class DetailsPromotion extends React.Component {
@@ -31,6 +32,7 @@ class DetailsPromotion extends React.Component {
                 <th>Prénom</th>
                 <th>Absences</th>
                 <th>Notes</th>
+                <th></th>
               </tr>
             </thead>
             <tbody>
@@ -38,8 +40,25 @@ class DetailsPromotion extends React.Component {
                 <tr key={student.id}>
                   <td> {student.firstname} </td>
                   <td> {student.lastname} </td>
-                  <td> 0 <img className="icon-plus" src={ plus } alt="ajouter une absence" /></td>
-                  <td> 0 <img className="icon-plus" src={ plus } alt="ajouter une note" /></td>
+                  <td>
+                    {/* data missing for absence */}
+                    0
+                    <Link to={`/absence/${student.id}`}>
+                      <img className="icon-plus" src={ plus } alt="ajouter une absence" />
+                    </Link>
+                  </td>
+                  <td>
+                    {/* data missing for notes */}
+                    0
+                    <Link to={`/grades/${student.id}`}>
+                      <img className="icon-plus" src={ plus } alt="ajouter une note" />
+                    </Link>
+                  </td>
+                  <td className='icon-access'>
+                    <Link to={`/student/${student.id}`}>
+                      >
+                    </Link>
+                  </td>
                 </tr>
               ))}
             </tbody>
