28f158fc6ea2116020133137e9e1bf5e8515d3ad
florian <zobeleflorian@gmail.com>
2018-05-10 00:23:54 +0200

    reorganization is done now

--- src/modules/Ues/components/Details/Details.components.jsx
+++ src/modules/Ues/components/Details/Details.components.jsx
@@ -1,6 +1,20 @@
 import React from 'react';
+import Proptypes from 'prop-types';
+
+import { default as ListCourses } from '@Courses/components/List/List.components';
 
 class Details extends React.Component {
+
+  static propTypes = {
+    id: Proptypes.number.isRequired,
+    name: Proptypes.string.isRequired,
+    description: Proptypes.string.isRequired,
+    courses: Proptypes.arrayOf(Proptypes.shape({
+      id: Proptypes.number.isRequired,
+      name: Proptypes.string.isRequired,
+      description: Proptypes.string.isRequired
+    })).isRequired
+  };
 
   render() {
     const { id, name, description, courses } = this.props;
@@ -10,7 +24,7 @@ class Details extends React.Component {
         <h1>UE : {name}</h1>
         <p>Desc : {description}</p>
         <div className="courses">
-          {/* <ListCourses /> */}
+          <ListCourses courses={courses} />
         </div>
       </div>
     );
