4c3402bd14a91ea892c16ca8a2bc7bef70e7f400
Lila <laladu13400@hotmail.fr>
2018-05-10 20:25:00 +0200

    list student using react-table

--- src/modules/Promos/components/Details/Details.components.jsx
+++ src/modules/Promos/components/Details/Details.components.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Proptypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import ReactTable from 'react-table';
 
 import plus from '@images/icon-plus.png';
 import Filter from '@Shared/containers/Filter.containers';
 
+import 'react-table/react-table.css';
 import './styles.scss';
 
 class DetailsPromotion extends React.Component {
@@ -35,53 +37,50 @@ class DetailsPromotion extends React.Component {
 
   render() {
     const { promotion } = this.props;
+    const columns = [
+      {Header: 'Nom', accessor: 'lastname'},
+      {Header: 'Prénom', accessor: 'firstname'},
+      {Header: 'Absences', accessor: 'absences', width: 75, className: 'centered-col',
+        Cell: row => (
+          <React.Fragment>
+            {row.value}
+            <Link to={`/grades/${row.row.username}`}>
+              <img className="icon-plus" src={plus} alt="ajouter une note" />
+            </Link>
+          </React.Fragment>
+        )
+      },
+      {Header: 'Notes', accessor: 'grades', width: 75,
+        Cell: row => (
+          <React.Fragment>
+            {row.value}
+            <Link to={`/grades/${row.row.username}`}>
+              <img className="icon-plus" src={plus} alt="ajouter une note" />
+            </Link>
+          </React.Fragment>
+        )
+      },
+      {Header: 'Page', accessor: 'username', width: 50,
+        Cell: row => (<span className='icon-access'><Link to={`/students/${row.value}`}> > </Link> </span>)}
+    ];
 
     return (
       <React.Fragment>
         <div className="promotions">
           <h1>Détails d'une promo</h1>
 
+
           <div className="flex justify-content-sb">
             <section className="section liste">
               <Filter placeholder="Rechercher un étudiant" />
-              <table>
-                <thead>
-                  <tr>
-                    <th>Nom</th>
-                    <th>Prénom</th>
-                    <th>Absences</th>
-                    <th>Notes</th>
-                    <th></th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {promotion.map(student => (
-                    <tr key={student.id}>
-                      <td> {student.firstname} </td>
-                      <td> {student.lastname} </td>
-                      <td>
-                        {/* data missing for absence */}
-                        0
-                        <Link to={`/absences/${student.id}`}>
-                          <img className="icon-plus" src={plus} alt="ajouter une absence" />
-                        </Link>
-                      </td>
-                      <td>
-                        {/* data missing for notes */}
-                        0
-                        <Link to={`/grades/${student.id}`}>
-                          <img className="icon-plus" src={plus} alt="ajouter une note" />
-                        </Link>
-                      </td>
-                      <td className='icon-access'>
-                        <Link to={`/students/${student.username}`}>
-                          >
-                        </Link>
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
+              <ReactTable
+                data={promotion}
+                columns={columns}
+                showPagination={false}
+                className="-highlight"
+                resizable={false}
+              />
+
             </section>
 
             <section className="section actions">
