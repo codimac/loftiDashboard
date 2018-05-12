d6a7bcd37e910a79d03e95a3b93855914285c6ea
Lila <laladu13400@hotmail.fr>
2018-05-12 16:42:51 +0200

    list students with onClick
    need to get the absences infos on one student now

--- src/modules/Absences/components/List/List.components.jsx
+++ src/modules/Absences/components/List/List.components.jsx
@@ -1,21 +1,71 @@
 import React from 'react';
 import Proptypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import store from '@App/App.store';
+import ReactTable from 'react-table';
+
+import { getPromotion } from '@Promos/reducers/details.reducers';
+
+import * as promotionsDetailsEffects from '@Promos/effects/details.effects';
 
 class List extends React.Component {
 
   static propTypes = {
-    getAbsencesList: Proptypes.func.isRequired
+    getAbsencesList: Proptypes.func.isRequired,
+    year: Proptypes.number.isRequired,
+    promotion: Proptypes.arrayOf(Proptypes.shape({
+      id: Proptypes.number.isRequired,
+      firstname: Proptypes.string.isRequired,
+      lastname: Proptypes.string.isRequired,
+      username: Proptypes.string.isRequired
+    })).isRequired,
+    match: Proptypes.shape({
+      params: Proptypes.shape({
+        id: Proptypes.string.isRequired
+      }).isRequired
+    }).isRequired,
   };
 
   componentDidMount() {
     this.props.getAbsencesList();
+    store.dispatch(promotionsDetailsEffects.getPromotion(this.props.match.params.promotionId));
+  }
+
+  getStudentDetails(event, id) {
+    console.log(id);
   }
 
   render() {
+    const { promotion } = this.props;
+    const {year} = this.props;
+
+    const columns = [
+      {Header: 'Nom', accessor: 'lastname'},
+      {Header: 'Prénom', accessor: 'firstname'},
+      {Header: 'Page', accessor: 'id', width: 50, className: 'centered-col',
+        Cell: row => (<span role='none' className='icon-access' onClick={(e) => this.getStudentDetails(e, row.row.id)}> > </span>)}
+    ];
+    const len = promotion.length;
     return (
       <React.Fragment>
-        List
+        <div className="absences">
+          <h1>Les absences de la promos {year}</h1>
+          <div className="flex flex-wrap-reverse justify-content-sb">
+            <section className="alig-items-start">
+              <ReactTable
+                defaultPageSize={len}
+                data={promotion}
+                noDataText="Aucun élève trouvé."
+                columns={columns}
+                showPagination={false}
+                className="-highlight"
+                resizable={false}
+                pageSize={len}
+              />
+            </section>
+          </div>
+
+        </div>
       </React.Fragment>
     );
   }
--- src/modules/Absences/containers/List.containers.jsx
+++ src/modules/Absences/containers/List.containers.jsx
@@ -3,9 +3,14 @@ import { bindActionCreators } from 'redux';
 import List from '@Absences/components/List/List.components';
 
 import { getAbsencesList } from '@Absences/reducers/list.reducers';
+import { getPromotion } from '@Promos/reducers/details.reducers';
+
 import * as actions from '@Absences/effects/list.effects';
 
-const mapStateToProps = state => getAbsencesList(state);
+const mapStateToProps = state => ({
+  year: getPromotion(state).year,
+  promotion: getPromotion(state).promotion,
+});
 
 const mapDispatchToProps = dispatch => bindActionCreators(actions, dispatch);
 
