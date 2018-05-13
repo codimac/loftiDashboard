c7db8de759bc460f872ad758b136c4674b8fe556
Lila <laladu13400@hotmail.fr>
2018-05-13 16:48:26 +0200

    podium displayed \o/

--- src/modules/Absences/components/PodiumStudents/PodiumStudents.components.jsx
+++ src/modules/Absences/components/PodiumStudents/PodiumStudents.components.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Proptypes from 'prop-types';
+import './style';
 
 class PodiumStudients extends React.Component {
 
@@ -7,12 +8,47 @@ class PodiumStudients extends React.Component {
     this.props.getPromoPodiumAbsences();
   }
 
+  renderPodium() {
+    const {absencesPodium} = this.props;
+    const podium = absencesPodium.slice(0, 3);
+    return (
+      <React.Fragment>
+        {podium.length > 0 &&
+          <React.Fragment>
+            <li>
+              {podium[1].firstname} {podium[1].lastname}
+              <div className="p-second">
+                2
+              </div>
+            </li>
+            <li>
+              {podium[0].firstname} {podium[0].lastname}
+              <div className="p-first">
+                1
+              </div>
+            </li>
+            <li>
+              {podium[2].firstname} {podium[2].lastname}
+              <div className="p-third">
+                3
+              </div>
+            </li>
+          </React.Fragment>
+        }
+      </React.Fragment>
+    );
+  }
   render() {
-    // console.log(this.props);
+    const {absencesPodium} = this.props;
+    const podium = absencesPodium.slice(0, 3);
 
     return (
       <div>
         le podium des meilleurs absents
+        <ol className="podium">
+          {this.renderPodium()}
+
+        </ol>
       </div>
     );
   }
--- src/modules/Absences/mocks/promoPodium.mocks.jsx
+++ src/modules/Absences/mocks/promoPodium.mocks.jsx
@@ -1,7 +1,7 @@
 const createFakeStudents = (number = 10) => {
   const students = Array(number);
   for (let i = 0; i < students.length; i++) {
-    students[i] = { id: i, firstname: `name${i}`, lastname: `surname${i}`, username: `username${i}`, absences: Math.floor(Math.random() * 30), grades: Math.floor(Math.random() * 20)};
+    students[i] = { id: i, firstname: `name${i}`, lastname: `surname${i}`, username: `username${i}`, absences: Math.floor(Math.random() * 30)};
   }
   students.push({id: number+1, firstname: 'François', lastname: 'Hollande', username: 'fhollande'});
   return students;
