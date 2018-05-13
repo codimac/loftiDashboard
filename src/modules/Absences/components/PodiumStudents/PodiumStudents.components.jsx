8103f9e505232d3e11f98e6e7a2fd28f60764561
Lila <laladu13400@hotmail.fr>
2018-05-13 18:42:36 +0200

    liste after the 3 best absents + style

--- src/modules/Absences/components/PodiumStudents/PodiumStudents.components.jsx
+++ src/modules/Absences/components/PodiumStudents/PodiumStudents.components.jsx
@@ -11,28 +11,35 @@ class PodiumStudients extends React.Component {
   renderPodium() {
     const {absencesPodium} = this.props;
     const podium = absencesPodium.slice(0, 3);
+    const others = absencesPodium.slice(4);
     return (
       <React.Fragment>
         {podium.length > 0 &&
           <React.Fragment>
-            <li>
+            <li className="li-podium">
               {podium[1].firstname} {podium[1].lastname}
               <div className="p-second">
                 2
               </div>
             </li>
-            <li>
+            <li className="li-podium">
               {podium[0].firstname} {podium[0].lastname}
               <div className="p-first">
                 1
               </div>
             </li>
-            <li>
+            <li className="li-podium">
               {podium[2].firstname} {podium[2].lastname}
               <div className="p-third">
                 3
               </div>
             </li>
+            {others.map(student => (
+              <li>
+                {student.firstname} {student.lastname}
+              </li>
+              )
+            )}
           </React.Fragment>
         }
       </React.Fragment>
