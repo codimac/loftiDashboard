efc151a29b3efb67bc795db43ea5a6720afa70fb
florian <zobeleflorian@gmail.com>
2018-05-18 01:45:09 +0200

    buttons and animations

--- src/modules/App/components/Login/Login.components.jsx
+++ src/modules/App/components/Login/Login.components.jsx
@@ -17,6 +17,7 @@ class Login extends React.Component {
     this.state = {
       username: '',
       password: '',
+      validForm: false,
       submitted: false
     };
   }
@@ -37,8 +38,16 @@ class Login extends React.Component {
     }
   }
 
+  validForm = () => {
+    this.setState({
+      validForm: this.state.username.length > 0 && this.state.password.length > 0
+    });
+  }
+
   handleChange = ev => {
-    this.setState({[ev.target.name]: ev.target.value});
+    this.setState({
+      [ev.target.name]: ev.target.value
+    }, () => this.validForm());
   }
 
   render() {
@@ -50,7 +59,7 @@ class Login extends React.Component {
           <form className="form" onSubmit={this.handleSubmit}>
             <Input className="full-size mb-2" type="text" placeholder="Identifiant" name="username" value={username} onChange={this.handleChange} />
             <Input className="full-size mb-2" type="password" placeholder="Mot de passe" name="password" value={password} onChange={this.handleChange} />
-            <button className="button button__white" type="submit">Connexion</button>
+            <button className="button" disabled={!this.state.validForm} type="submit"><span className="wave wave-buttons"></span>Connexion</button>
           </form>
         </section>
       </div>
--- src/modules/Shared/components/Input/Input.components.jsx
+++ src/modules/Shared/components/Input/Input.components.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import Proptypes from 'prop-types';
+
 import './Input.styles';
 
 class Input extends React.Component {
 
+  static propTypes = {
+    type: Proptypes.string.isRequired,
+    name: Proptypes.string,
+    className: Proptypes.string,
+    value: Proptypes.string,
+    onChange: Proptypes.func,
+    placeholder: Proptypes.string
+  };
+
   render() {
-    console.log(this.props);
     const { type, name, className, value, onChange, placeholder } = this.props;
     return (
       <div className={`${className} input__animated`}>
