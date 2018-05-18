f24f751ec188f3b160bdc3ac4606ccb25c5897e5
florian <zobeleflorian@gmail.com>
2018-05-18 02:49:04 +0200

    button proptypes

--- src/modules/Shared/components/Button/Button.components.jsx
+++ src/modules/Shared/components/Button/Button.components.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import Proptypes from 'prop-types';
 
 class Button extends React.Component {
 
+  static propTypes = {
+    className: Proptypes.string,
+    disabled: Proptypes.bool,
+    type: Proptypes.string,
+    children: Proptypes.node.isRequired
+  };
+
   render() {
-    console.log(this.props);
-    const { className, disabled, type } = this.props;
+    const { className, disabled, type, children } = this.props;
     return (
-      <button className={className} disabled={disabled} type={type}><span className="wave wave-buttons"></span>Connexion</button>
+      <button className={className} disabled={disabled} type={type}><span className="wave wave-buttons"></span>{children}</button>
     );
   }
 
