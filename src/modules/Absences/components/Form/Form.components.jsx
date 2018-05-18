e1eb9bb0e2056571e2b5bac8a0f4045fbb3b2de1
Lila <laladu13400@hotmail.fr>
2018-05-18 20:24:12 +0200

    add a line input to add multiple absences at once

--- src/modules/Absences/components/Form/Form.components.jsx
+++ src/modules/Absences/components/Form/Form.components.jsx
@@ -2,6 +2,14 @@ import React from 'react';
 import SelectInput from '@Shared/components/SelectInput/SelectInput.components';
 
 class Form extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      nbToAdd: 1,
+    };
+    this.addAbsencesInput = this.addAbsencesInput.bind(this);
+    this.deleteAbsencesInput = this.deleteAbsencesInput.bind(this);
+  }
   oneAbsence() {
     return (
       <React.Fragment>
@@ -9,18 +17,36 @@ class Form extends React.Component {
         <label htmlFor="justified"> Justification
           <input id="justified" type="checkbox" name="justified" />
         </label>
+        <button onClick={this.deleteAbsencesInput}>X</button>
       </React.Fragment>
 
     );
   }
 
+  addAbsencesInput() {
+    this.setState({nbToAdd: this.state.nbToAdd+1});
+  }
+  deleteAbsencesInput() {
+    if (this.state.nbToAdd > 0) {
+      this.setState({nbToAdd: this.state.nbToAdd-1});
+    }
+  }
 
   render() {
+    const nbAbs = [];
+    for (let i = 0; i < this.state.nbToAdd; i++) {
+      nbAbs.push(
+        <div key={i}>
+          {this.oneAbsence()}
+        </div>);
+    }
     return (
       <React.Fragment>
         <h3>Ajouter une absences </h3>
         {this.oneAbsence()}
-        <button onClick={this.oneAbsence} >Ajouter une autre absences </button>
+
+        { nbAbs }
+        <button onClick={this.addAbsencesInput} >Ajouter une autre absences </button>
         <button type="submit">Enregistrer </button>
       </React.Fragment>
 
